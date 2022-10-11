2b4a43a76da21b7e7762b3b5e4b443b5b00a4c14
azamjon <azamjonisakov0214@gmail.com>
2022-10-11 21:31:48 +0500

    header menu

---
+++ src/components/lib/HeaderItem.vue
@@ -0,0 +1,31 @@
+<template>
+    <header class="header">
+        <router-link to="/" class="logo">
+            <img src="@/assets/img/logo.jpg" alt="logo">
+        </router-link>
+        <ul class="header__menu">
+            <li><router-link to="/">kursi</router-link></li>
+            <li><router-link to="/">biz haqimizda</router-link></li>
+            <li><router-link to="/">blog</router-link></li>
+            <li>
+                <span>yana</span>
+                <ul class="header__dropdown">
+                    <li><router-link to="/">FAQ</router-link></li>
+                    <li><router-link to="/">Kontact</router-link></li>
+                    <li><router-link to="/">Tolovlar</router-link></li>
+                </ul>
+            </li>
+        </ul>
+        <button class="btn">Kirish</button>
+    </header>
+</template>
+
+<script>
+export default {
+    components: {}
+}
+</script>
+
+<style lang="scss"> 
+    @import '@/styles/components/header.scss';
+</style>
